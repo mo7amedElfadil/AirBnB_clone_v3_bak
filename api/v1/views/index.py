3ff0f2f46b5516c0f1f66959f38dda06e9d874f0
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-29 20:20:18 +0200

    reorder import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Index module"""
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
