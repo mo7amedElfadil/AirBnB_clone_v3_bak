ec64c5d8e6c1498217f9073771a3e43d032e28dd
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-29 21:40:19 +0200

    idc

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,11 @@
 """
 Flask web api
 """
+
 from os import getenv
 from flask import Flask, jsonify
-from api.v1.views import app_views
 from models import storage
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
@@ -25,10 +26,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=getenv("HBNB_API_PORT", 5000),
+            threaded=True)
