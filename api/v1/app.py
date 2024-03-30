c7b47335c3c2e6afb384b998d4077730002de54a
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-30 14:22:03 +0400

    removing global strict slahes

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+# app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
