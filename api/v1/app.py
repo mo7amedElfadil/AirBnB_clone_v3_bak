53213f62625c23d2849430a2e86c157d02260b96
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-03-30 13:52:21 +0400

    giving cors a variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,17 +3,17 @@
 Flask web api
 """
 
+from models import storage
 from api.v1.views import app_views
+from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
-from models import storage
-from os import getenv
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
