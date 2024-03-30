5a3d3d0845ccb26f76ca84a34d6e99085152059e
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-30 06:01:25 +0200

    testing rm cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,6 @@ Flask web api
 
 from os import getenv
 from flask import Flask, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
@@ -13,7 +12,6 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
