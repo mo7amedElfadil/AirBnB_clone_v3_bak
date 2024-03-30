85aa7ccb9a9f1af4508a3655ec0ed1808eedaa41
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-30 20:22:57 +0200

    asdjhaduuf

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,22 +2,21 @@
 """
 Flask web api
 """
-
 from os import getenv
 from flask import Flask, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_storage(_=None):
+def close_storage(_):
     """Close storage"""
     storage.close()
 
@@ -29,6 +28,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=getenv("HBNB_API_PORT", 5000),
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', 5000)),
             threaded=True)
