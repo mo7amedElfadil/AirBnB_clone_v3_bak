12488cbf6a5226d6489d22b3644c075f4713a160
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-31 22:59:00 +0200

    nullable time

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,9 @@ Base = declarative_base()
 
 class BaseModel:
     """BaseModel class"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow())
 
     def __init__(self, *_, **kwargs):
         """Instantiate an instance"""
