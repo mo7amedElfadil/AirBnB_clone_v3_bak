d3c54bfd07917a2978278d18b727f178b40f860c
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-03-31 23:38:07 +0200

    idk

--- models/base_model.py
+++ models/base_model.py
@@ -61,5 +61,3 @@ class BaseModel:
         """Instance representaion"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
-
-
